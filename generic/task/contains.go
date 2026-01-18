1410f1cfc84fbf1198197ef8c45fefc96c4edc66
Andrew <asoborev@mail.ru>
2026-01-18 22:48:53 +0300

    contains это типы которые можно комплексно создать самому

--- generic/task/contains.go
+++ generic/task/contains.go
@@ -1,11 +1,9 @@
 package task
 
-// Проверить, есть ли элемент в слайсе.
-func Contains[T comparable](arr []T, v T) bool {
-	for _, a := range arr {
-		if a == v {
-			return true
-		}
-	}
-	return false
+type NumberType interface {
+	int | float64 | int64
+}
+
+func Sum[T NumberType](value1, value2 T) T {
+	return value1 + value2
 }
