706985d5c45553afd55d5604053663a06447786c
Andrew <asoborev@mail.ru>
2026-01-22 20:20:15 +0300

    оптимизировал тестирование min, сделав именованые тесты

--- testing/list/list_test.go
+++ testing/list/list_test.go
@@ -1,23 +1,28 @@
 package list
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMin(t *testing.T) {
 	// варианты последовательностей
-	lists := [][]int{
-		{5, -50, 67},
-		{-7, 1, 43, 100},
-		{3, 4},
-		{10},
-		{},
-	}
+	// первая цифра массива это контрольное значение, остальные проверочные
+    lists := map[string][]int{
+        "three": {-50, 5, -50, 67},
+        "four":  {-7, -7, 1, 43, 100},
+        "two":   {3, 3, 4},
+        "one":   {10, 10},
+        "empty": {0},
+    }
 
 	// ожидаемые значения для каждой последовательности
-	mins := []int{-50, -7, 3, 10, 0}
-
-	for i, list := range lists {
-		if Min(list) != mins[i] {
-			t.Error(i, ":", Min(list), "!=", mins[i])
-		}
-	}
+   for name, list := range lists {
+        t.Run(name, func(t *testing.T) {
+            if Min(list[1:]) != list[0] {
+                t.Errorf("get %d != want %d", Min(list[1:]), list[0])
+            }
+        })
+    }
 }
+
+ 
