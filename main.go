a979b3d5ec646e9a7998edf73aed4485f3b67b44
Andrew <asoborev@mail.ru>
2026-02-12 20:50:58 +0300

    чистка не нужных файлов

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "goTour/feature_postgres/simple_connection"
+import (
+	"sync"
+)
+
+type Job struct {
+	index int
+	value int
+}
+
+func RunWorkers(jobs []int, workerCount int) []int {
+	result := make([]int, len(jobs))
+	wg := &sync.WaitGroup{}
+	jobChan := make(chan Job)
+
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			for job := range jobChan {
+				result[job.index] = job.value * job.value
+			}
+		}()
+	}
+
+	for i, v := range jobs {
+		jobChan <- Job{
+			index: i,
+			value: v,
+		}
+	}
+
+	close(jobChan)
+	wg.Wait()
+
+	return result
+}
 
 func main() {
-	simple_connection.CheckConnection()
+
+	RunWorkers([]int{1, 2, 3}, 2)
 }
