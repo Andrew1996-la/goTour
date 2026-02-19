0c9e5dc0275538cc88ab3b323b1cd208d55b4f89
Andrew <asoborev@mail.ru>
2026-02-19 21:49:12 +0300

    пример разделения асинхронного кода и бизнес логики say

---
+++ gorutines/paractice/separation.go
@@ -0,0 +1,34 @@
+package paractice
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"sync"
+	"time"
+)
+
+func say(id int, phrase string) {
+	for _, word := range strings.Fields(phrase) {
+		fmt.Printf("Worker# %d says: %s\n", id, word)
+		dur := time.Duration(rand.Intn(100)) * time.Millisecond
+		time.Sleep(dur)
+	}
+}
+
+func separation() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		say(1, "go is awesome")
+	}()
+
+	go func() {
+		defer wg.Done()
+		say(2, "cats are cute")
+	}()
+
+	wg.Wait()
+}
