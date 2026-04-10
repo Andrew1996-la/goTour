b6935ef00175f33c29b3c71c39406d62856f9610
Andrew <asoborev@mail.ru>
2026-04-10 19:41:11 +0300

    increment with atomic

---
+++ gorutines/paractice/baseAtomic.go
@@ -0,0 +1,25 @@
+package paractice
+
+import (
+    "fmt"
+    "sync"
+    "sync/atomic"
+)
+
+func baseAtomicTest() {
+    var wg sync.WaitGroup
+    var counter atomic.Uint64
+
+    for i := 0; i < 10; i++ {
+        wg.Add(1)
+
+        go func() {
+            defer wg.Done()
+            for j := 0; j < 100000; j++ {
+				counter.Add(1)
+            }
+        }()
+    }
+    wg.Wait()
+    fmt.Println(counter.Load())
+}
